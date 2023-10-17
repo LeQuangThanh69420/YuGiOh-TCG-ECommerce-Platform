da72bc1e5af14562d918f48e85482a52d4154c19
HoangDilo <truonghoang14122003@gmail.com>
2023-10-17 22:41:47 +0700

    test get API

--- FE/src/Components/Body.jsx
+++ FE/src/Components/Body.jsx
@@ -1,17 +1,33 @@
+import { useEffect, useState } from 'react'
 import './Body.css'
 
-
 function Body() {
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5233/api/Card/searchCard')
+            .then(res => res.json())
+            .then(data => {setCards(data)})
+    }, [])
+
+    console.log(cards);
+
     return (
         <div className='body-session'>
             < div className="body-container" >
-                <div className='cards'></div>
-                <div className='cards'></div>
-                <div className='cards'></div>
-                <div className='cards'></div>
-                <div className='cards'></div>
-                <div className='cards'></div>
-                <div className='cards'></div>
+                {
+                    cards.map((item, index) => 
+                        <div className='cards' key={index}>
+                            <div className='card-name'>
+                                {item.cardName}
+                            </div>
+                            <img src={item.cardImageURL} alt="" className='cards-img'/>
+                            <div className='rarity'>
+                                {item.cardRarityName}
+                            </div>
+                        </div>
+                    )
+                }
             </div >
         </div>
     )
