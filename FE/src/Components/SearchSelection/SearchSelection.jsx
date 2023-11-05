fcb7b9a94fba40c0e59ee3ff44de96b3d32d5005
HoangDilo <129303901+HoangDilo@users.noreply.github.com>
2023-11-05 22:40:00 +0700

    change FE format files

--- FE/src/Components/SearchSelection/SearchSelection.jsx
+++ FE/src/Components/SearchSelection/SearchSelection.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from 'react'
-import './SearchSelection.css'
+import './../../styles/SearchSelection.css'
 
 
 function SearchSelection({ type, selections, propSet, onSelect }) {
