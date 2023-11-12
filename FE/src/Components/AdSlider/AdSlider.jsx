c2a9dca440ffe73aa737a272e28e4bd7bd799031
LeeHaii <93501285+LeeHaii@users.noreply.github.com>
2023-11-13 00:23:33 +0700

    design gan xong
    chit chit

--- FE/src/Components/AdSlider/AdSlider.jsx
+++ FE/src/Components/AdSlider/AdSlider.jsx
@@ -1,28 +1,29 @@
 import { useEffect, useState } from 'react'
 import ImageSlider from './ImageSlider'
 import './../../styles/AdSlider.css'
-import AdTest1 from './../../asset/AdSliderTestImage1.png'
-import AdTest2 from './../../asset/AdSliderTestImage2.png'
-import AdTest3 from './../../asset/AdSliderTestImage.png'
-import AdTest4 from './../../asset/AdSliderTestImage1.png'
-import AdTest5 from './../../asset/AdSliderTestImage2.png'
-import AdsTest from './../../asset/AdSliderSideAdsTestImage.png'
+import Ad1 from './../../constants/SliderLoginNow.png'
+import Ad2 from './../../constants/SliderRegisterNow.png'
+import Ad3 from './../../constants/SliderTryLuck.png'
+import Ad4 from './../../constants/SliderAboutUs.png'
+import Ad5 from './../../asset/AdSliderTestImage2.png'
+import SmallAd1 from './../../constants/SmallAd1BEWD.png'
+import SmallAd2 from './../../constants/SmallAd2Laby.png'
 
 function AdSlider(){
-    const slides = [AdTest1,AdTest2,AdTest3,AdTest4,AdTest5,]
+    const slides = [Ad1,Ad2,Ad3,Ad4,Ad5,]
 
     return(
         <>  
             <div className="AdSlider">
                 <div className="AdSlider-Slider">
-                    <ImageSlider slides={slides}/>
+                    <ImageSlider slides={slides} />
                 </div>
                 <div className="AdSlider-Ads">
                     <div className="AdSlider-Ads-FirstAdElement">
-                        <img src={AdsTest} alt="chitnhau" />
+                        <img src={SmallAd1} alt="chitnhau" />
                     </div>
                     <div className="AdSlider-Ads-SecondAdElement">
-                        <img src={AdsTest} alt="chitnhau" />
+                        <img src={SmallAd2} alt="chitnhau" />
                     </div>
                 </div>
             </div>
