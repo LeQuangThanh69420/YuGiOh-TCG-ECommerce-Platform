28b3abf3066d85840fd3a5b5455345a5517013e5
LeeHaii <93501285+LeeHaii@users.noreply.github.com>
2023-12-03 00:04:48 +0700

    gacha
    你好吗

--- FE/src/constants/gachaBannerInfo.js
+++ FE/src/constants/gachaBannerInfo.js
@@ -1,13 +1,15 @@
 import img1 from './../asset/gachaBannerImg1.png'
+import img2 from './../asset/gachaBannerImg2.png'
+import img3 from './../asset/gachaBannerImg3.png'
 
 export const banner = [
     {name: 'Standard',
     img: img1,
     price: 900},
     {name: 'Deluxe',
-    img: '',
+    img: img2,
     price: 4500},
     {name: 'Waifu',
-    img: '',
+    img: img3,
     price: 1800}
 ]
