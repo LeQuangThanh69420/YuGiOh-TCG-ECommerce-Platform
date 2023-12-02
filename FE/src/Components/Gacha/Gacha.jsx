71c3088a1da445cce7c4d35f3463fa817788a3b0
LeeHaii <93501285+LeeHaii@users.noreply.github.com>
2023-12-02 15:50:42 +0700

    sech
    sech

--- FE/src/Components/Gacha/Gacha.jsx
+++ FE/src/Components/Gacha/Gacha.jsx
@@ -1,10 +1,32 @@
 import Header from "../Shared/Header"
 import "../../styles/Gacha.css"
+import { banner } from "../../constants/gachaBannerInfo"
+
 
 export default function Gacha() {
     return (
         <>
-            <div>Gacha</div>
+            <div className="Gacha">
+                <div className="Gacha-container">
+                    <div className="Gacha-pulling-screen">
+                        <div className="Gacha-pulling-background">
+                            <div className="Gacha-pulling-background-curve">
+                                <div className="Gacha-pulling-background-dots">
+                                    <div className="Gacha-pulling-img" style={{backgroundImage: `url(${banner[0].img})`}}></div>
+                                    <div className="Gacha-pulling-price-button">
+                                        <div className="Gacha-pulling-price"></div>
+                                        <button className="Gacha-pulling-pull">Pull Card x10</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="Gacha-background">
+                    <div className="Gacha-background-front"></div>
+                    <div className="Gacha-background-behind"></div>
+                </div>
+            </div>
         </>
     )
 }
---
+++ FE/src/constants/gachaBannerInfo.js
@@ -0,0 +1,10 @@
+import img1 from './../asset/gachaBannerImg1.png'
+
+export const banner = [
+    {name: 'Standard',
+    img: img1,},
+    {name: 'Deluxe',
+    img: '',},
+    {name: 'Waifu',
+    img: '',}
+]
