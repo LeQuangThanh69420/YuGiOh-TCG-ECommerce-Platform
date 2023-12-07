1655de946df41134d233e54be216dadd91eff303
le quang then <lequanhlol@gmail.com>
2023-12-07 23:13:27 +0700

    add env

---
+++ BE/Model/ValueObject/ApiEnvironment.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BE.Model.ValueObject
+{
+    public class ApiEnvironment
+    {
+        public const string ClientURL = "http://localhost:5173";
+        public const int GachaPrice = 100;
+    }
+}
