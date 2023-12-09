759189a86cb29630fcb3601640215ad385323c27
HoangDilo <129303901+HoangDilo@users.noreply.github.com>
2023-12-09 22:12:46 +0700

    update cards seperate, stack

--- FE/src/Components/User/UserCards.jsx
+++ FE/src/Components/User/UserCards.jsx
@@ -13,39 +13,70 @@ export default function UserCards() {
   const { userData } = useContext(AppData)
 
   const [cardOwned, setCardOwned] = useState([]);
+
   const [displayCards, setDisplayCards] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
   const [cardSelected, setCardSelected] = useState();
   const [currentPage, setCurrentPage] = useState(1);
+  const [isStack, setIsStack] = useState();
 
   const handleOpenDetail = (card) => {
-    setIsOpen(true)
-    setCardSelected(card);
-    console.log(card);
+    if (card.quantity > 0) {
+      setIsOpen(true)
+      setCardSelected(card);
+    }
+  }
+
+  const handleToggleViewOption = () => {
+    setIsStack(!isStack);
+    setCurrentPage(1);
   }
 
   useEffect(() => {
-    getOwnedCardsSeperate(userData.username).then(data => {
-      setCardOwned(data);
-    })
-  }, [])
+    if (isStack) {
+      getOwnedCardsStack(userData.username).then(data => {
+        setCardOwned(data);
+      })
+    } else {
+      getOwnedCardsSeperate(userData.username).then(data => {
+        setCardOwned(data);
+        setIsStack(false);
+      })
+    }
+  }, [isStack])
 
   return (
     <>
       <div className="user-cards-wrapper">
-        <p className="user-cards-title">
-          <span className="text-second">Your </span>
-          <span className="text-primary">Cards</span>
-        </p>
+        <div className="user-cards-title">
+          <p>
+            <span className="text-secondary">Your </span>
+            <span className="text-primary">Cards</span>
+          </p>
+          <div className="user-cards-view-option" onClick={handleToggleViewOption}>
+            <div className={`${!isStack ? 'cards-stack' : 'cards-seperate'} icon-8`}>
+            </div>
+            {!isStack ?
+              <span className="text-third">Stack</span> :
+              <span className="text-third">Seperate</span>
+            }
+          </div>
+
+        </div>
+
         <div className="user-cards-container">
           {displayCards.map((card, index) =>
-            <div className="cards" key={index} onClick={() => handleOpenDetail(card)}>
+            <div className={`cards ${!card.quantity && 'not-owned'}`} key={index} onClick={() => handleOpenDetail(card)}>
+              {card.onDeal && <div className="cards-on-deal">Selling</div>}
               <div className={`rarity ${card.cardRarityName}`}>{card.cardRarityName}</div>
-              <img src={card.cardImageURL} className="cards-img" />
+              <div className="cards-img-wrapper">
+                <img src={card.cardImageURL} className="cards-img" />
+                <div className="cards-quantity">{card.quantity}</div>
+              </div>
             </div>
           )}
         </div>
-        <Pagination currentPage={currentPage} list={cardOwned} numberItem={10} setCurrentPage={setCurrentPage} setPagedList={setDisplayCards}/>
+        <Pagination currentPage={currentPage} list={cardOwned} numberItem={10} setCurrentPage={setCurrentPage} setPagedList={setDisplayCards} />
       </div>
       <CardDetails isOpen={isOpen} selectedCard={cardSelected} onClose={() => setIsOpen(false)} />
     </>
