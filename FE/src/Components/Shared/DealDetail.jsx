a15998898b306697595a46fd183286b0dd778696
HoangDilo <129303901+HoangDilo@users.noreply.github.com>
2023-12-16 11:51:58 +0700

    refactor code cua thang

--- FE/src/Components/Shared/DealDetail.jsx
+++ FE/src/Components/Shared/DealDetail.jsx
@@ -10,69 +10,63 @@ const DealDetails = ({ isOpen, selectedDeal, onClose, onBuy }) => {
     else return 'Bucac'
   }
 
-  function checkDate(selectedDeal){
-    var date = new Date(selectedDeal.createDate);
-  }
-
-  if (!isOpen) return null;
-
   return (
     <>
-    <div className="DealDetails">
-      <div className="DealDetails-content">
-        <div className="DealDetails-head">
-          <div className="DealDetails-DuRiu">
-            <span className="text-secondary">Deal</span>{" "}
-            <span className="text-primary">Details</span>
+      {isOpen && <div className="DealDetails" onClick={() => onClose()}>
+        <div className="DealDetails-content" onClick={(event) => {event.stopPropagation()}}>
+          <div className="DealDetails-head">
+            <div className="DealDetails-DuRiu">
+              <span className="text-secondary">Deal</span>{" "}
+              <span className="text-primary">Details</span>
+            </div>
+            <div className="DealDetails-close close-icon icon-5" onClick={onClose} ></div>
           </div>
-          <div className="DealDetails-close close-icon icon-5" onClick={onClose} ></div>
-        </div>
-        <div className="DealDetails-body">
-          <div className="DealDetails-pic-preview">
-            <div className="DealDetails-img" style={{ backgroundImage: `url(${selectedDeal.cardImageURL})` }}></div>
-          </div>
-          <div className="DealDetails-details-container">
-            <div className="DealDetails-details-body-head">
+          <div className="DealDetails-body">
+            <div className="DealDetails-pic-preview">
+              <div className="DealDetails-img" style={{ backgroundImage: `url(${selectedDeal.cardImageURL})` }}></div>
+            </div>
+            <div className="DealDetails-details-container">
+              <div className="DealDetails-details-body-head">
                 <img src={selectedDeal.sellUsernameAvatarUrl} className="header-user-avt-display" style={{ width: '50px', height: '50px' }} />
-              <div className="DealDetails-details-body-head-info">
-                <span className="DealDetails-sell-user text-third">{selectedDeal.sellUsername}</span>
-                <div className="DealDetails-name-info-date text-forth">{selectedDeal.createDate}</div>
+                <div className="DealDetails-details-body-head-info">
+                  <span className="DealDetails-sell-user text-third">{selectedDeal.sellUsername}</span>
+                  <div className="DealDetails-name-info-date text-forth">{selectedDeal.createDate}</div>
+                </div>
               </div>
-            </div>
-            <div className="DealDetails-details-body-body">
-              <div className="DealDetails-details">
-                <div className="DealDetails-label text-secondary">Rarity: </div>
-                <div className="DealDetails-name-info text-third">{checkRarity(selectedDeal)}</div>
+              <div className="DealDetails-details-body-body">
+                <div className="DealDetails-details">
+                  <div className="DealDetails-label text-secondary">Rarity: </div>
+                  <div className="DealDetails-name-info text-third">{checkRarity(selectedDeal)}</div>
+                </div>
+                <div className="DealDetails-details-name">
+                  <div className="DealDetails-label text-secondary">Card Name: </div>
+                  <div className="DealDetails-name-info text-third">{selectedDeal.cardName}</div>
+                </div>
+                <div className="DealDetails-details">
+                  <div className="DealDetails-label text-secondary">Card Element: </div>
+                  <div className="DealDetails-info text-third">{selectedDeal.cardElementName == null ? "None" : selectedDeal.cardElementName}</div>
+                </div>
+                <div className="DealDetails-details">
+                  <div className="DealDetails-label text-secondary">Card Origin: </div>
+                  <div className="DealDetails-info text-third">{selectedDeal.cardOriginName == null ? "None" : selectedDeal.cardOriginName}</div>
+                </div>
               </div>
-              <div className="DealDetails-details-name">
-                <div className="DealDetails-label text-secondary">Card Name: </div>
-                <div className="DealDetails-name-info text-third">{selectedDeal.cardName}</div>
-              </div>
-              <div className="DealDetails-details">
-                <div className="DealDetails-label text-secondary">Card Element: </div>
-                <div className="DealDetails-info text-third">{selectedDeal.cardElementName == null ? "None" : selectedDeal.cardElementName}</div>
-              </div>
-              <div className="DealDetails-details">
-                <div className="DealDetails-label text-secondary">Card Origin: </div>
-                <div className="DealDetails-info text-third">{selectedDeal.cardOriginName == null ? "None" : selectedDeal.cardOriginName}</div>
-              </div>
-            </div>
-            <div className="DealDetails-details-body-bottom">
-              <div className="DealDetails-bottom-price">
-                <span className='text-sixth deal-price'>{selectedDeal.price}</span>
-                <div className='riu-coin-icon icon-9'></div>
+              <div className="DealDetails-details-body-bottom">
+                <div className="DealDetails-bottom-price">
+                  <span className='text-sixth deal-price'>{selectedDeal.price}</span>
+                  <div className='riu-coin-icon icon-9'></div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="DealDetails-bottom">
-          <div className="DealDetails-bottom-buttons">
-            <button className="DealDetails-bottom-cancel" onClick={onClose}>Cancel</button>
-            <button className="DealDetails-bottom-buy" onClick={onBuy}>Buy</button>
+          <div className="DealDetails-bottom">
+            <div className="DealDetails-bottom-buttons">
+              <button className="DealDetails-bottom-cancel" onClick={onClose}>Cancel</button>
+              <button className="DealDetails-bottom-buy" onClick={onBuy}>Buy</button>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </div>}
     </>
   );
 };
