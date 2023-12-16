1a218e18e6d39b4f8ab800f539847834614823a6
HoangDilo <129303901+HoangDilo@users.noreply.github.com>
2023-12-16 10:19:35 +0700

    update confirm modal function

--- FE/src/Components/Shared/ConfirmModal.jsx
+++ FE/src/Components/Shared/ConfirmModal.jsx
@@ -10,7 +10,7 @@ export default function ConfirmModal({ isOpen = false, title, content, setIsOpen
                     <p className='confirm-modal-title'>{title}</p>
                     <p className='confirm-modal-content'>{content}</p>
                     <div className='confirm-modal-buttons'>
-                        <button className='button-1'>Cancel</button>
+                        <button className='button-1' onClick={() => setIsOpen(false)}>Cancel</button>
                         <button onClick={onOK}>OK</button>
                     </div>
                     <div className='close-icon icon-4' onClick={() => setIsOpen(false)}></div>
