6218044d8858f9a6b2ed35b58a27eb2bf950a87a
t06112003 <130381074+t06112003@users.noreply.github.com>
2023-12-16 23:30:09 +0700

    labrynth gacha

--- BE/Controllers/GachaController.cs
+++ BE/Controllers/GachaController.cs
@@ -18,6 +18,7 @@ namespace BE.Controllers
         private readonly DataContext _context;
         private readonly int normalPrice = ApiEnvironment.normalPrice;
         private readonly int deluxePrice = ApiEnvironment.deluxePrice;
+        private readonly int waifuPrice = ApiEnvironment.waifuPrice;
         public GachaController(DataContext context)
         {
             _context = context;
@@ -70,6 +71,27 @@ namespace BE.Controllers
                 }
             }
 
+            if(input.Pack == "waifu")
+            {
+                if(user.Money >= waifuPrice)
+                {
+                    user.Money -= waifuPrice;
+                    _context.SaveChanges();
+                }
+                else return BadRequest(new {message = "Account don't have enough money!"});
+
+                for (int i = 0; i < 10; i++)
+                {
+                    var rate = new Random().Next(1, 101);
+
+                    if (rate <= 40) randomCard = await GetRandomCardsLabrynth("N");
+                    else if (rate <= 70) randomCard = await GetRandomCardsLabrynth("R");
+                    else if (rate <= 95) randomCard = await GetRandomCardsLabrynth("SR");
+                    else randomCard = await GetRandomCardsLabrynth("UR");
+                    allCards.Add(randomCard);
+                }
+            }
+
             for (int i = 0; i < 10; i++)
             {
                 var card = allCards[i];
@@ -96,5 +118,15 @@ namespace BE.Controllers
                 .Take(1)
                 .SingleOrDefaultAsync();
         }
+
+        private async Task<Card> GetRandomCardsLabrynth(string rarity)
+        {
+            return await _context.Card
+                .Where(c => c.CardRarityName == rarity && c.CardName.Contains("Labrynth"))
+                .OrderBy(c => Guid.NewGuid())
+                .Take(1)
+                .SingleOrDefaultAsync();
+        }
+
     }
 }
--- BE/Model/ValueObject/ApiEnvironment.cs
+++ BE/Model/ValueObject/ApiEnvironment.cs
@@ -10,6 +10,7 @@ namespace BE.Model.ValueObject
         public const string ClientURL = "http://localhost:5173";
         public const int normalPrice = 1000;
         public const int deluxePrice = 5000;
+        public const int waifuPrice = 2000;
         public const int defRegMoney = 2000;
         public const string live2dCanvas = "<canvas id='live2d' width='560' height='600' class='live2d' style='position: absolute; bottom: 0px; left: 0%; width: 32%;'></canvas>";
         public const string imageDeptrai = "/assets/image/deptrai.png";
