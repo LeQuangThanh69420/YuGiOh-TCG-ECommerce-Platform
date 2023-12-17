ccf98ff51b2766ee232c19ab66ea90541199d29d
HoangDilo <129303901+HoangDilo@users.noreply.github.com>
2023-12-17 12:53:16 +0700

    edit remove logging

--- FE/src/utils/dateTimeFormat.js
+++ FE/src/utils/dateTimeFormat.js
@@ -1,5 +1,4 @@
 export default function dateTimeFormat(dateTime) {
-    console.log(dateTime)
     if (dateTime) {
         const date = dateTime.substring(0, 10).split('-').reverse().join('-');
         const time = dateTime.substring(11);
