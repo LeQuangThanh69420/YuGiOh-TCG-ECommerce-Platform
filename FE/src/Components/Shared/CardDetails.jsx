87e457ceffb081e19080bf4e404c000a0c536a9c
HoangDilo <129303901+HoangDilo@users.noreply.github.com>
2023-12-18 23:24:34 +0700

    edit shits

--- FE/src/Components/Shared/CardDetails.jsx
+++ FE/src/Components/Shared/CardDetails.jsx
@@ -79,6 +79,7 @@ const CardDetails = ({ isOpen, selectedCard, onClose, isManaging = false, onAddD
               }
             </div>
           </div>
+          <iframe src={`http://localhost:5233/api/Deal/GetCardPrice/${selectedCard.cardId}/${new Date().getFullYear()}`} className='card-price-iframe'></iframe>
         </div>
       </div>}
     </>
